bb64d9fe9bd6febe5b69655f3540883c583e7997
anco-mochi <anco-mochi@mac-book-air.local>
2020-09-14 02:22:31 +0900

    fix: add addReadableUser variable in index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,9 @@
 import * as functions from 'firebase-functions';
+import * as memoFunctions from './memoFunctions';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+export const addReadableUser = memoFunctions.addReadableUser;
